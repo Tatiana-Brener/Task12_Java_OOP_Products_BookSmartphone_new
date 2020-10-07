c0684bca77b00ee489caa37cab6341a43dbca43f
Tatana <burmatova@inbox.ru>
2020-10-07 22:12:22 +0300

    Deleted method "matches" from ProductManager.java

--- src/test/java/ru/netology/manager/ProductManagerTest.java
+++ src/test/java/ru/netology/manager/ProductManagerTest.java
@@ -1,6 +1,5 @@
 package ru.netology.manager;
 
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
